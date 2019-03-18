33c0b1e1778cfc9fbf8e455a45a29b2c38c0d6cb
jleogier <jleogier@users.noreply.github.com>
2019-03-18 14:35:26 -0700

    Add files via upload

--- experiment.js
+++ experiment.js
@@ -2,5 +2,27 @@ $( document ).ready(function() {
     console.log("Get funky");
 
 
+    $('body').on('click', '#projIcon1', function (e) {  
+        console.log('Project 1 Icon Button was clicked')              
+
+        return $('.projectDescrip1').toggle();
+
+
+        });    
+
+    
+    $('body').on('click', '#aboutMeButton', function (e) {  
+        console.log('About Me Button was clicked')      
+
+        //.fadeIn('5000') <-- Need to make it fade, doesn't work correctly
+
+        return $('#aboutMe').toggle();
+
+
+        });                
+    
+
+
+
 
 });
